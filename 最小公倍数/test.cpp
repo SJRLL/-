4453ad5f4e0750b209e0c3060cdd0dedc08e6730
SJRLL <1124335040@qq.com>
2019-11-12 19:20:11 +0800

    //牛客网---最小公倍数

---
+++ 最小公倍数/test.cpp
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//#include<vector>
+//#include<stdlib.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b;
+//	cin >> a >> b;
+//	int mua = a*b;
+//
+//	while (b)
+//	{
+//		int tmp = a%b;
+//		a = b;
+//		b = tmp;
+//	}
+//	cout << mua / a << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+
